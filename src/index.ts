0fab190ce136fd100a592b91e65c61fc28a26f4d
“zhaodongxu” <hualuna87@126.com>
2021-09-08 14:40:30 +0800

    feat: test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Vane
  * @Date: 2021-08-19 19:06:06
- * @LastEditTime: 2021-09-08 14:33:45
+ * @LastEditTime: 2021-09-08 14:38:36
  * @LastEditors: Vane
  * @Description: 入口
  * @FilePath: \tp-cli\src\index.ts
