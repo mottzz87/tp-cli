b74b16780fe75c3d80214e63f4c1710a7352521d
“zhaodongxu” <hualuna87@126.com>
2021-09-08 15:06:02 +0800

    feat: test

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /*
  * @Author: Vane
  * @Date: 2021-08-19 19:06:06
- * @LastEditTime: 2021-09-08 14:55:35
+ * @LastEditTime: 2021-09-08 14:57:09
  * @LastEditors: Vane
  * @Description: 入口
  * @FilePath: \tp-cli\src\index.ts
